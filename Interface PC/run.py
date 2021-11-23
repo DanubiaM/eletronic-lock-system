d77dd7c67cf5ea3af23e535308b6477052b56bd2
DanubiaM <dubamacedoo@gmail.com>
2021-11-23 01:03:38 -0400

    refactor: initialize in Login

--- Interface PC/run.py
+++ Interface PC/run.py
@@ -1,10 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QDialog
 from modules.login import Login
+from modules.mainWindow import MainWindow
 
 aplicacao = QApplication(sys.argv)
 app = QApplication(sys.argv)
 if (QDialog.Accepted == True):
     window = Login()
+    #window = MainWindow()
     window.show()
 sys.exit(app.exec_())
